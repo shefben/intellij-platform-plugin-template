56559e409600378cffc385471ec27157ecef056f
shefben <shefben@gmail.com>
2025-06-20 06:03:31 -0500

    Add dialog resizing and context menus

---
+++ src/main/kotlin/org/jetbrains/plugins/template/tkdesigner/ui/ComponentPalette.kt
@@ -0,0 +1,44 @@
+package org.jetbrains.plugins.template.tkdesigner.ui
+
+import java.awt.GridLayout
+import javax.swing.JButton
+import javax.swing.JDialog
+
+/**
+ * Floating palette with Tkinter widgets.
+ */
+class ComponentPalette(private val design: DesignAreaPanel) : JDialog() {
+    init {
+        title = "Widgets"
+        layout = GridLayout(0, 2, 2, 2)
+        isAlwaysOnTop = true
+        isResizable = false
+
+        val types = listOf(
+            "Button",
+            "Label",
+            "Entry",
+            "Text",
+            "Frame",
+            "Canvas",
+            "Menu",
+            "Menubutton",
+            "PanedWindow",
+            "Scrollbar",
+            "Checkbutton",
+            "Radiobutton",
+            "Listbox",
+            "Scale",
+            "Spinbox"
+        )
+        for (t in types) {
+            add(createButton(t))
+        }
+
+        pack()
+    }
+
+    private fun createButton(type: String): JButton = JButton(type).apply {
+        addActionListener { design.beginAddWidget(type) }
+    }
+}
